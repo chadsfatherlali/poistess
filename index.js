0c09f8a73e1d54303c891dab044fda3c08a748d5
Chasfather <chadsfather@gmail.com>
2015-09-25 22:27:18 +0200

    Puntos de ventas para TESS

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * Created by chadsfather on 25/9/15.
+ */
+
+var express = require('express'),
+     swig = require('swig'),
+     app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.engine('html', swig.renderFile);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.set('view cache', false);
+
+app.get('/', function(request, response) {
+     response.render('pages/index', {vars: {test: 'SANTIAGO'}});
+});
+
+app.listen(app.get('port'), function() {
+     console.log('Node app is running on port', app.get('port'));
+});
+
+
+
