450b11f70fc016d571f57d84f2b579f3f6b0e998
Chasfather <chadsfather@gmail.com>
2015-09-27 19:25:53 +0200

    Implementacion de propia modulo para google maps

---
+++ public/js/angularMapModule.js
@@ -0,0 +1,82 @@
+/**
+ * Created by chadsfather on 27/9/15.
+ */
+
+/**
+ * Retorna la latitud y la longitud desde el API de Google
+ *
+ * @param $http
+ * @param $q
+ * @param $log
+ * @returns {{parse: Function}}
+ */
+function parseGeoPoints ($http, $q, $log) {
+     return {
+          parse: function (value) {
+               var deferred = $q.defer();
+
+               $http.get('http://maps.google.com/maps/api/geocode/json?address=' + value + '&sensor=false')
+                    .success(function (data) {
+                         deferred.resolve(data);
+                    })
+                    .error(function (msg, code) {
+                         $log.error(msg, code);
+                    });
+
+               return deferred.promise;
+          }
+     }
+}
+
+/**
+ * Directiva que se encarga de renderizar el mapa
+ *
+ * @param $rootScope
+ * @param parseGeoPoints
+ * @returns {{restrict: string, scope: {mapPoints: string, mapHeight: string, mapWidth: string, mapZoom: string}, link: Function}}
+ */
+function map ($rootScope, parseGeoPoints) {
+     return {
+          restrict: 'E',
+          scope: {
+               mapPoints: '@points',
+               mapHeight: '@height',
+               mapWidth: '@width',
+               mapZoom: '@zoom'
+          },
+          link: function(scope, element, attrs) {
+               var height = scope.mapHeight || '300px';
+               var width = scope.mapWidth || '500px';
+               var zoom = scope.mapZoom || 7;
+
+               $rootScope.el = element[0];
+
+               $rootScope.el.style.display = 'block';
+               $rootScope.el.style.height = height;
+               $rootScope.el.style.width = width;
+
+               parseGeoPoints.parse(scope.mapPoints).then(function (data) {
+                    var latlng = data.results;
+                    var lastPoint = latlng.length - 1;
+
+                    new google.maps.Map($rootScope.el, {
+                         center: {
+                              lat: latlng[lastPoint].geometry.location.lat,
+                              lng: latlng[lastPoint].geometry.location.lng
+                         },
+                         scrollwheel: true,
+                         zoom: zoom
+                    });
+               });
+          }
+     }
+}
+
+/**
+ * Modulo para Dibujar mapas con el API de google maps,
+ * depende de la libreria de google
+ * Link: https://maps.google.com/maps/api/js
+ */
+angular.module('maps', [])
+     .factory('parseGeoPoints', ['$http', '$q', '$log', parseGeoPoints])
+     .directive('map', ['$rootScope', 'parseGeoPoints', map]);
--- public/js/app.js
+++ public/js/app.js
@@ -6,115 +6,166 @@
 
 var app = angular.module('StarterApp', [
      'ngMaterial',
-     'ngMap'
+     'maps'
 ]);
 
 app.config(['$interpolateProvider', function ($interpolateProvider) {
      $interpolateProvider.startSymbol('[[').endSymbol(']]');
 }]);
 
-function AppCtrl ($scope) {
+function AppCtrl($scope, $mdSidenav, $mdUtil) {
+     $scope.local = {};
+
+     $scope.toggleLeft = buildToggler('left');
+     $scope.toggleRight = buildToggler('right');
+
      $scope.oficinas = [
           {
                'localidad': 'Quito',
-               'tipo': 'Local',
-               'local': 'Tess A-121',
-               'nombre': 'Centro comercial Iñaquito C.C.I.',
-               'direccion': 'Av. AMAZONAS N36-152 Y Av. NACIONES UNIDAS',
-               'telefono': '02-2277720',
-               'email': 'cci@ingesa.com.ec',
-               'geo': '-0.1730965,-78.483365',
-               'zoom': 7
+               'centros': [
+                    {
+                         'tipo': 'Local',
+                         'local': 'Tess A-121',
+                         'nombre': 'Centro comercial Iñaquito C.C.I.',
+                         'direccion': 'Av. AMAZONAS N36-152 Y Av. NACIONES UNIDAS',
+                         'telefono': '02-2277720',
+                         'email': 'cci@ingesa.com.ec',
+                         'geo': '-0.1730965,-78.483365',
+                         'zoom': 7
+                    },
+                    {
+                         'localidad': 'Quito',
+                         'tipo': 'Isla',
+                         'local': 'TESS P1-K40',
+                         'nombre': 'Quicentro Shopping',
+                         'direccion': 'Av. NACIONES UNIDAS S/N y SEIS DE DICIEMBRE',
+                         'telefono': '02-3824002',
+                         'email': 'quicentro@ingesa.com.ec',
+                         'geo': '-0.1761243,-78.4802341',
+                         'zoom': 7
+                    },
+                    {
+                         'localidad': 'Quito',
+                         'tipo': 'Local',
+                         'local': 'TESS J-54',
+                         'nombre': 'Centro comercial el Recreo',
+                         'direccion': 'Av. MALDONADO S/N y CALLE S/N',
+                         'telefono': '02-2668054',
+                         'email': 'recreo@ingesa.com.ec',
+                         'geo': '-0.2525642,-78.5231569',
+                         'zoom': 7
+                    }
+               ]
           },
           {
-               'localidad': 'Quito',
-               'tipo': 'Isla',
-               'local': 'TESS P1-K40',
-               'nombre': 'Quicentro Shopping',
-               'direccion': 'Av. NACIONES UNIDAS S/N y SEIS DE DICIEMBRE',
-               'telefono': '02-3824002',
-               'email': 'quicentro@ingesa.com.ec',
-               'geo': '-0.1761243,-78.4802341',
-               'zoom': 7
-          },
-          {
-               'localidad': 'Quito',
-               'tipo': 'Local',
-               'local': 'TESS J-54',
-               'nombre': 'Centro comercial el Recreo',
-               'direccion': 'Av. MALDONADO S/N y CALLE S/N',
-               'telefono': '02-2668054',
-               'email': 'recreo@ingesa.com.ec',
-               'geo': '-0.2525642,-78.5231569',
-               'zoom': 7
-          },
-          {
+
                'localidad': 'Valle de los Chillos',
-               'tipo': 'Isla',
-               'local': 'TESS I2-013',
-               'nombre': 'Centro comercial San Luis',
-               'direccion': 'CALLE SANTA CLARA Y Av. GENERAL RUMIÑAHUI',
-               'telefono': '02-2090315',
-               'email': 'sanluis@ingesa.com.ec',
-               'geo': '-0.3076142,-78.4499721',
-               'zoom': 7
+               'centros': [
+                    {
+                         'tipo': 'Isla',
+                         'local': 'TESS I2-013',
+                         'nombre': 'Centro comercial San Luis',
+                         'direccion': 'CALLE SANTA CLARA Y Av. GENERAL RUMIÑAHUI',
+                         'telefono': '02-2090315',
+                         'email': 'sanluis@ingesa.com.ec',
+                         'geo': '-0.3076142,-78.4499721',
+                         'zoom': 7
+                    }
+               ]
           },
           {
                'localidad': 'Guayaquil',
-               'tipo': 'Local',
-               'local': 'TESS No. 29',
-               'nombre': 'Paseo Shopping - Duran',
-               'direccion': 'Km. 3 1/2 - AUTOPISTA DURAN-BOLICHE',
-               'telefono': '04-3886025',
-               'email': 'duran@ingesa.com.ec',
-               'geo': '-2.1789414,-79.8246778',
-               'zoom': 7
-          },
-          {
-               'localidad': 'Guayaquil',
-               'tipo': 'Local',
-               'local': 'TESS No. 76',
-               'nombre': 'La Peninsula - La Libertad',
-               'direccion': 'CENTRO COMERCIAL EL PASEO SHOPPINPG Local No. 76',
-               'telefono': '04-2775311',
-               'email': 'libertad@ingesa.com.ec',
-               'geo': '-2.2259415,-80.9211355',
-               'zoom': 7
+               'centros': [
+                    {
+                         'tipo': 'Local',
+                         'local': 'TESS No. 29',
+                         'nombre': 'Paseo Shopping - Duran',
+                         'direccion': 'Km. 3 1/2 - AUTOPISTA DURAN-BOLICHE',
+                         'telefono': '04-3886025',
+                         'email': 'duran@ingesa.com.ec',
+                         'geo': '-2.1789414,-79.8246778',
+                         'zoom': 7
+                    },
+                    {
+                         'tipo': 'Local',
+                         'local': 'TESS No. 76',
+                         'nombre': 'La Peninsula - La Libertad',
+                         'direccion': 'CENTRO COMERCIAL EL PASEO SHOPPINPG Local No. 76',
+                         'telefono': '04-2775311',
+                         'email': 'libertad@ingesa.com.ec',
+                         'geo': '-2.2259415,-80.9211355',
+                         'zoom': 7
+                    },
+               ]
           },
           {
                'localidad': 'Latacunga',
-               'tipo': 'Isla',
-               'local': 'TESS PBK 03',
-               'nombre': 'Centro comercial Malteria',
-               'direccion': 'Av. ELOY ALFARO Y GATAZO',
-               'telefono': '03-2279074',
-               'email': 'riobamba@ingesa.com',
-               'geo': '-0.9256063,-78.6260425',
-               'zoom': 7
+               'centros': [
+                    {
+                         'tipo': 'Isla',
+                         'local': 'TESS PBK 03',
+                         'nombre': 'Centro comercial Malteria',
+                         'direccion': 'Av. ELOY ALFARO Y GATAZO',
+                         'telefono': '03-2279074',
+                         'email': 'riobamba@ingesa.com',
+                         'geo': '-0.9256063,-78.6260425',
+                         'zoom': 7
+                    }
+               ]
           },
           {
                'localidad': 'Riobamba',
-               'tipo': 'Local',
-               'local': 'TESS No. 86',
-               'nombre': 'Paseo Shopping Riobamba',
-               'direccion': 'Av. ANTONIO JOSE DE SUCRE Y BEGONIAS',
-               'telefono': '03-2310072',
-               'email': 'riobamba@ingesa.com',
-               'geo': '-1.6539851,-78.6461315',
-               'zoom': 7
+               'centros': [
+                    {
+                         'tipo': 'Local',
+                         'local': 'TESS No. 86',
+                         'nombre': 'Paseo Shopping Riobamba',
+                         'direccion': 'Av. ANTONIO JOSE DE SUCRE Y BEGONIAS',
+                         'telefono': '03-2310072',
+                         'email': 'riobamba@ingesa.com',
+                         'geo': '-1.6539851,-78.6461315',
+                         'zoom': 7
+                    }
+               ]
           },
           {
                'localidad': 'Babahoyo',
-               'tipo': 'Local',
-               'local': 'TESS No. 45',
-               'nombre': 'Centro comercial Paseo Shopping',
-               'direccion': 'Av. ENRIQUE PONCE LUQUE',
-               'telefono': '05-2789019',
-               'email': 'babahoyo@ingesa.com.ec',
-               'geo': '-1.8140289,-79.5459294',
-               'zoom': 7
+               'centros': [
+                    {
+                         'tipo': 'Local',
+                         'local': 'TESS No. 45',
+                         'nombre': 'Centro comercial Paseo Shopping',
+                         'direccion': 'Av. ENRIQUE PONCE LUQUE',
+                         'telefono': '05-2789019',
+                         'email': 'babahoyo@ingesa.com.ec',
+                         'geo': '-1.8140289,-79.5459294',
+                         'zoom': 7
+                    }
+               ]
           },
      ];
+
+     function buildToggler(navID) {
+          var debounceFn = $mdUtil.debounce(function () {
+               $mdSidenav(navID)
+                    .toggle()
+                    .then(function () {
+                    });
+          }, 200);
+
+          return debounceFn;
+     }
+
+     $scope.close = function () {
+          $mdSidenav('right').close()
+               .then(function () {
+                    $log.debug("close LEFT is done");
+               });
+     };
+
+     $scope.buscar = function() {
+          console.log('null');
+     }
 }
 
-app.controller('AppCtrl', ['$scope', AppCtrl]);
+app.controller('AppCtrl', ['$scope', '$mdSidenav', '$mdUtil', AppCtrl]);
